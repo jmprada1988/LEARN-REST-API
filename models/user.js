c98133b31aae488c49723a5763d77c17eae76731
davirlein1988 <lein.davir@gmail.com>
2020-01-14 01:55:01 -0500

    added the initial user schema

---
+++ models/user.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  passwrod: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  courses: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Course'
+    }
+  ],
+  programs: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Program'
+    }
+  ],
+  role: {
+    type: String,
+    default: 'New User'
+  },
+  avatar: {
+    type: String,
+  },
+  status: {
+    type: String,
+    default: 'Active'
+  },
+  course_certificates: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Course'
+    }
+  ],
+  program_certificates: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Program'
+    }
+  ]
+});
+
+module.exports = mongoose.model('User', userSchema);
